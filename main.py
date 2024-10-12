b4bd5f9474b47209fc1834a647a6eae6cd224fa8
vkSegfault <adtofaust@gmail.com>
2024-10-12 16:43:52 +0200

    upd

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # should be DB here
 with open('jobs.json') as f:
